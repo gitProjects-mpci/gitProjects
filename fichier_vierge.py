812cdc7cb1a11ee6c0c89860a26bc2198671c719
uvsq22000713 <71426172+uvsq22000713@users.noreply.github.com>
2020-12-10 08:58:36 +0100

    Add files via upload

---
+++ fichier_vierge.py
@@ -0,0 +1,67 @@
+import tkinter as tk 
+
+def q():
+    racine.destroy()
+
+if __name__ == '__main__':
+    
+
+    racine = tk.Tk()
+
+    bouton1 = tk.Button(racine, text = " 1 ", activebackground = "black",
+                         height = 2, width = 4, bg = "gray69"          
+                       )
+    bouton1.grid(column = 0, row = 3) 
+
+    bouton2 = tk.Button(racine, text = " 2 ", activebackground = "black",
+                         height = 2, width = 4, bg = "gray69"          
+                       )
+    bouton2.grid(column = 1, row = 3) 
+
+    bouton3 = tk.Button(racine, text = " 3 ", activebackground = "black",
+                         height = 2, width = 4, bg = "gray69"          
+                       )
+    bouton3.grid(column = 2, row = 3) 
+
+    bouton4 = tk.Button(racine, text = " 4 ", activebackground = "black",
+                         height = 2, width = 4, bg = "gray69"          
+                       )
+    bouton4.grid(column = 0, row = 2) 
+
+    bouton5 = tk.Button(racine, text = " 5 ", activebackground = "black",
+                         height = 2, width = 4, bg = "gray69"          
+                       )
+    bouton5.grid(column = 1, row = 2) 
+
+    bouton6 = tk.Button(racine, text = " 6 ", activebackground = "black",
+                         height = 2, width = 4, bg = "gray69"          
+                       ) 
+    bouton6.grid(column = 2, row = 2) 
+
+    bouton7 = tk.Button(racine, text = " 7 ", activebackground = "black",
+                         height = 2, width = 4, bg = "gray69"          
+                       )
+    bouton7.grid(column = 0, row = 1) 
+
+    bouton8 = tk.Button(racine, text = " 8 ", activebackground = "black",
+                         height = 2, width = 4, bg = "gray69"          
+                       )
+    bouton8.grid(column = 1, row = 1) 
+
+    bouton9 = tk.Button(racine, text = " 9 ", activebackground = "black",
+                         height = 2, width = 4, bg = "gray69"          
+                       )
+    bouton9.grid(column = 2, row = 1)
+
+    ecran = tk.Entry(racine, text = "Saisir...", width = 20)
+    ecran.grid(column =0, row = 0, columnspan = 3)
+
+
+
+
+
+    quitter = tk.Button(racine, text = "quitter", command = q, height =1, width = 15)
+    quitter.grid(column = 0, row = 5, columnspan = 3)
+   
+    racine.configure(bg = "gray10")
+    racine.mainloop()

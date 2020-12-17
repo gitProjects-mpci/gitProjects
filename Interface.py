beac50a2182d42af418874caa1aa931bc22953b4
uvsq22000713 <sam-khong-lucas-rasamizanany@ens.uvsq.fr>
2020-12-17 10:43:33 +0100

    interface

--- Interface.py
+++ Interface.py
@@ -8,8 +8,9 @@ def expression(nombres):
 
 def egal():
     global operateur
-    operateur = eval(operateur)
+    operateur = str(eval(operateur))
     text_input.set(operateur)
+    operateur = ""
 
 def q():
     racine.destroy()
@@ -25,7 +26,7 @@ if __name__ == '__main__':
     operateur = ""
     text_input = StringVar()
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------
     #boutons numéraires
     bouton1 = Button(racine, text = " 1 ", activebackground = "black", height = 2, width = 4,bg = "gray69",
                     command = lambda:expression(1)).grid(column = 0, row = 3)
@@ -33,18 +34,21 @@ if __name__ == '__main__':
                     command = lambda:expression(2)).grid(column = 1, row = 3) 
     bouton3 = Button(racine, text = " 3 ", activebackground = "black", height = 2, width = 4,bg = "gray69",
                     command = lambda:expression(3)).grid(column = 2, row = 3) 
+#---------------------------------------------------------------------------------------------------------------------------------------
     bouton4 = Button(racine, text = " 4 ", activebackground = "black", height = 2, width = 4,bg = "gray69",
                     command =lambda:expression(4)).grid(column = 0, row = 2)
     bouton5 = Button(racine, text = " 5 ", activebackground = "black", height = 2, width = 4,bg = "gray69",
                     command = lambda:expression(5)).grid(column = 1, row = 2) 
     bouton6 = Button(racine, text = " 6 ", activebackground = "black", height = 2, width = 4,bg = "gray69",
                     command = lambda:expression(6)).grid(column = 2, row = 2) 
+#---------------------------------------------------------------------------------------------------------------------------------------
     bouton7 = Button(racine, text = " 7 ", activebackground = "black", height = 2, width = 4,bg = "gray69",
                     command = lambda:expression(7)).grid(column = 0, row = 1) 
     bouton8 = Button(racine, text = " 8 ", activebackground = "black", height = 2, width = 4,bg = "gray69",
                     command = lambda:expression(8)).grid(column = 1, row = 1) 
     bouton9 = Button(racine, text = " 9 ", activebackground = "black", height = 2, width = 4,bg = "gray69",
                     command = lambda:expression(9)).grid(column = 2, row = 1)
+#---------------------------------------------------------------------------------------------------------------------------------------
     bouton0 = Button(racine, text = " 0 ", activebackground = "black", height = 2, width = 4,bg = "gray69",
                     command = lambda:expression(0)).grid(column = 1, row = 4)
     blankbutton = Button(racine, text = "   ", state = 'disabled', height = 2, width = 4,bg = "gray69").grid(column = 0, row = 4)
@@ -53,7 +57,7 @@ if __name__ == '__main__':
     #ecran
     ecran = Entry(racine, text = "Saisir...", width = 31, textvariable = text_input).grid(column =0, row = 0, columnspan = 5)
 
-
+#---------------------------------------------------------------------------------------------------------------------------------------
     #opérateurs
     virg = Button(racine, text = " . ", activebackground = "black", height = 2, width = 4,bg = "gray69",
                     command = lambda:expression(".")).grid(column = 2, row = 4)
@@ -62,16 +66,17 @@ if __name__ == '__main__':
     minus = Button(racine, text = " - ", activebackground = "gray90",height = 2, width = 4, bg = "gray50",
                     command = lambda:expression("-")).grid(column = 3, row = 2)
     mul = Button(racine, text = " * ", activebackground = "gray90",height = 2, width = 4, bg = "gray50",
-                    command = lambda:expression("x")).grid(column = 3, row = 3)
+                    command = lambda:expression("*")).grid(column = 3, row = 3)
     div = Button(racine, text = " / ", activebackground = "gray90",height = 2, width = 4, bg = "gray50",
                     command = lambda:expression("/")).grid(column = 3, row = 4)
     equal = Button(racine, text = " = ", activebackground = "gray90", height = 1, width = 4,bg = "gray50",
-                    command = lambda:egal).grid(column = 3, row = 5)
+                    command = egal).grid(column = 3, row = 5)
     supr = Button(racine, text = " C ", activebackground = "gray90", height = 2, width = 4,bg = "gray50",
                     command = delete).grid(column = 4, row = 1)
 
     quitter = Button(racine, text = "quitter", command = q, height =1, width = 15).grid(column = 0, row = 5, columnspan = 3)
-
+#---------------------------------------------------------------------------------------------------------------------------------------
+    
     #boucle 
     racine.title( "Calculatrice") 
     racine.configure(bg = "gray10")
